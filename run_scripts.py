4dc34263506330b9ca5a903b640d48953ba1b5c4
francisco-avalos <avalosjr.francisco@gmail.com>
2022-11-28 15:55:35 -0800

    chime alert and minor typo

--- run_scripts.py
+++ run_scripts.py
@@ -1,7 +1,11 @@
 import sys
-import subprocess
+import chime
+
+# import subprocess
+from subprocess import call
 
 
+chime.theme('mario')
 small_list=['advanced_stats.py', 'high_level_nba_team_schedules.py',
 				'high_level_nba_team_stats.py', 'my_team_stats.py']
 full_list=['advanced_stats.py', 'high_level_nba_team_schedules.py',
@@ -10,15 +14,17 @@ full_list=['advanced_stats.py', 'high_level_nba_team_schedules.py',
 # free_agents_alone='free_agents_backfill.py'
 
 
-run_fa = input('Execute Free Agents Scrtipt?(yes/no) - ')
+run_fa = input('Execute Free Agents Script?(yes/no) - ')
 
 if run_fa=='yes':
 	print('Begin full list now... \n')
 	for script in full_list:
-		subprocess.call(['python', script])
-		print('Finished executing - ', script)
+		call(['python', script])
+		chime.success()
+		print('Finished executing - ', script, '\n')
 elif run_fa=='no':
 	print('Begin small list now... \n')
 	for script in small_list:
-		subprocess.call(['python', script])
-		print('Finished executing - ', script)
+		call(['python', script])
+		chime.success()
+		print('Finished executing - ', script, '\n')
