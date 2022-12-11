f64309c53573edce9afc884e95325e84a1e43d3a
francisco-avalos <avalosjr.francisco@gmail.com>
2022-12-11 13:28:06 -0800

    pull created functions from separate file

--- free_agents_backfill.py
+++ free_agents_backfill.py
@@ -17,7 +17,7 @@ import unidecode
 import time
 from datetime import date
 from datetime import timedelta
-
+from my_functions import clean_string, remove_name_suffixes
 
 
 ## Preliminaries, set ups & initiators 
@@ -32,74 +32,6 @@ league=League(league_id=leagueid,
 				espn_s2=espn_s2,
 				swid=swid, 
 				debug=False)
-
-
-### Definitions used
-def remove_team_string(item):
-	output = item.replace('Team(', '')
-	output = output.replace(')', '')
-	output = output.replace('[', '').replace(']','')
-	output.lstrip()
-	return output
-
-def remove_matchup_string(item):
-	output = item.replace('Matchup(', '')
-	output = output.replace(')', '')
-	output = output.replace('[', '').replace(']','')
-	output.lstrip()
-	return output
-
-def remove_activity_string(item):
-	output = item.replace('Activity(', '')
-	output = output.replace(')', '')
-	output = output.replace('[', '').replace(']','')
-	output.lstrip()
-	return output
-
-
-def remove_player_string(item):
-	output = item.replace('Player(', '')
-	output = output.replace(')', '')
-	output = output.replace(', points:0', '')
-	output = output.replace('[', '').replace(']','')
-	output.lstrip()
-	return output
-
-def remove_box_string(item):
-	output = item.replace('Box Score(', '')
-	output = output.replace(')', '')
-	output = output.replace('[', '').replace(']','')
-	output.lstrip()
-	return output
-
-def clean_string(item):
-	item=str(item)
-	item=remove_team_string(item)
-	item=remove_matchup_string(item)
-	item=remove_activity_string(item)
-	item=remove_player_string(item)
-	item=remove_box_string(item)
-	return item
-
-def remove_name_suffixes(item):
-	item=str(item)
-	item=item.replace('Jr.','')
-	item=item.replace('II','')
-	item=item.replace('III','')
-	item=item.replace('IV','')
-	item=item.replace('Sr.','')
-	item=item.replace('Sr.','')
-	return item
-
-def convert_game_score_to_points(GS, FG, FGA, FTA, FT, ORB, DRB, STL, AST, BLK, PF, TOV):
-	"""
-		Obtain a player's points per game by
-		inverting John Hollinger's game score measure.
-		John Hollinger's Game Score = PTS + 0.4 * FG - 0.7 * FGA - 0.4*(FTA - FT) + 0.7 * ORB + 0.3 * DRB + STL + 0.7 * AST + 0.7 * BLK - 0.4 * PF - TOV. 
-	"""
-	PTS=GS - 0.4 * FG + 0.7 * FGA + 0.4*(FTA - FT) - 0.7 * ORB - 0.3 * DRB - STL - 0.7 * AST - 0.7 * BLK + 0.4 * PF + TOV
-	return PTS
-
 
 # myteam=league.teams[11]
 # my_players=clean_string(myteam.roster).split(',')
