af6260ddd3d116661f4e3fad12864a0ab4807210
francisco-avalos <avalosjr.francisco@gmail.com>
2022-12-13 22:52:08 -0800

    add historicals script and final sound completion confirmation

--- run_scripts.py
+++ run_scripts.py
@@ -7,24 +7,37 @@ from subprocess import call
 
 chime.theme('mario')
 small_list=['advanced_stats.py', 'high_level_nba_team_schedules.py',
-				'high_level_nba_team_stats.py', 'my_team_stats.py']
+				'high_level_nba_team_stats.py', 'my_team_stats.py',
+				'historicals.py']
 full_list=['advanced_stats.py', 'high_level_nba_team_schedules.py',
 				'high_level_nba_team_stats.py', 'my_team_stats.py',
+				'historicals.py',
 				'free_agents_backfill.py']
-# free_agents_alone='free_agents_backfill.py'
-
 
 run_fa = input('Execute Free Agents Script?(yes/no) - ')
 
 if run_fa=='yes':
-	print('Begin full list now... \n')
-	for script in full_list:
-		call(['python', script])
+	try:
+		print('Begin full list now... \n')
+		for script in full_list:
+			call(['python', script])
+			chime.success()
+			print('Finished executing - ', script, '\n')
+		chime.theme('zelda')
 		chime.success()
-		print('Finished executing - ', script, '\n')
+	except:
+		chime.theme('mario')
+		chime.error()
 elif run_fa=='no':
-	print('Begin small list now... \n')
-	for script in small_list:
-		call(['python', script])
+	try:
+		print('Begin small list now... \n')
+		for script in small_list:
+			call(['python', script])
+			chime.success()
+			print('Finished executing - ', script, '\n')
+		chime.theme('zelda')
 		chime.success()
-		print('Finished executing - ', script, '\n')
+	except:
+		chime.theme('mario')
+		chime.error()
+
