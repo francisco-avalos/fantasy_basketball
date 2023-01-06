75bdc7c198168f6fe4873b8c616ed85cfd9324de
francisco-avalos <avalosjr.francisco@gmail.com>
2023-01-05 23:07:47 -0600

    updating injured players

--- injured_report.py
+++ injured_report.py
@@ -12,13 +12,13 @@ from mysql.connector import Error
 exec(open('/Users/franciscoavalosjr/Desktop/basketball-creds.py').read())
 
 out_players=[
-'https://www.rotowire.com/basketball/player/dangelo-russell-3708',
+# 'https://www.rotowire.com/basketball/player/dangelo-russell-3708',
 # 'https://www.rotowire.com/basketball/player/tyrese-haliburton-5114',
 # # 'https://www.rotowire.com/basketball/player/jarrett-allen-4122',
 # 'https://www.rotowire.com/basketball/player/norman-powell-3726',
- 'https://www.rotowire.com/basketball/player/deandre-hunter-4776',
+ # 'https://www.rotowire.com/basketball/player/deandre-hunter-4776',
 # 'https://www.rotowire.com/basketball/player/tj-mcconnell-3760',
-'https://www.rotowire.com/basketball/player/jimmy-butler-3231',
+# 'https://www.rotowire.com/basketball/player/jimmy-butler-3231',
 # 'https://www.rotowire.com/basketball/player/gary-trent-4433',
 'https://www.rotowire.com/basketball/player/anthony-davis-3297',
 'https://www.rotowire.com/basketball/player/jrue-holiday-3029',
