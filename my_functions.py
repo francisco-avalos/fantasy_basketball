457285c6d7caac820df88e7dd33701bc52604458
francisco-avalos <avalosjr.francisco@gmail.com>
2023-04-03 21:58:46 -0700

    tracking my functions module

--- my_functions.py
+++ my_functions.py
@@ -1,3 +1,10 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import datetime as dt
+import time
+
+
 '''My functions'''
 
 ### Definitions used
@@ -74,3 +81,121 @@ def convert_game_score_to_points(GS, FG, FGA, FTA, FT, ORB, DRB, STL, AST, BLK, 
 	"""
 	PTS=GS - 0.4 * FG + 0.7 * FGA + 0.4*(FTA - FT) - 0.7 * ORB - 0.3 * DRB - STL - 0.7 * AST - 0.7 * BLK + 0.4 * PF + TOV
 	return PTS
+
+
+
+
+
+
+
+def list_of_pages(dat):
+	page_list=[]
+	df=dat.find_all('p',class_='bodyCopy')
+	for i in df:
+		row=i.find_all('a')
+		if row:
+			x=[y.text.strip() for y in row]
+			if x[0]!='Next':
+				x.insert(0,'1')
+				page_list=x.copy()
+	if page_list:
+		page_list=[eval(i) for i in page_list]
+		page_list=[i*25 for i in page_list]
+		page_list.insert(0,0)
+	elif not page_list:
+		page_list.insert(0,0)
+
+	return page_list
+
+def pagenize_url(my_list, url_input):
+	finished_list=[]
+	for page in my_list:
+		new_url=url_input+f'%22&start={page}'
+		finished_list.append(new_url)
+	return finished_list
+
+def day_injuries_basketball(day) -> pd.DataFrame():
+    original_url=f"https://www.prosportstransactions.com/basketball/Search/SearchResults.php?Player=&Team=&BeginDate={day}&EndDate={day}&ILChkBx=yes&InjuriesChkBx=yes&PersonalChkBx=yes&DisciplinaryChkBx=yes&LegalChkBx=yes&Submit=Search"
+    res=requests.get(original_url,timeout=None).content
+    data=BeautifulSoup(res,'html.parser')
+
+    pages_iteration=list_of_pages(data)
+    url_pagenated_list=pagenize_url(pages_iteration,original_url)
+    if len(url_pagenated_list)>1:
+    	url_pagenated_list=url_pagenated_list[:-1]
+    
+    time.sleep(5)
+    main_df=pd.DataFrame()
+    for url in url_pagenated_list:
+    	res=requests.get(url,timeout=None).content
+    	data=BeautifulSoup(res,'html.parser')
+
+    	table=data.find('table', class_='datatable center')
+    	rows=table.find_all('tr')
+    	mylist=[]
+    	for row in rows:
+    		cols=row.find_all('td')
+    		cols=[x.text.strip() for x in cols]
+    		mylist.append(cols)
+    	df=pd.DataFrame(mylist[1:],columns=['date','team','acquired','relinquished','notes'])
+    	clean_r=[x.replace('• ','') for x in df.relinquished]
+    	clean_a=[x.replace('• ','') for x in df.acquired]
+
+    	syntax_clean_list_r=[]
+    	for name in range(len(clean_r)):
+    		c_name=clean_r[name]
+    		s=c_name.encode().decode('unicode_escape').encode('raw_unicode_escape')
+    		string_uni=s.decode('utf-8')
+    		syntax_clean_list_r.append(string_uni)
+    	df.relinquished=syntax_clean_list_r
+
+    	syntax_clean_list_a=[]
+    	for name in range(len(clean_a)):
+    		c_name=clean_a[name]
+    		s=c_name.encode().decode('unicode_escape').encode('raw_unicode_escape')
+    		string_uni=s.decode('utf-8')
+    		syntax_clean_list_a.append(string_uni)
+    	df.acquired=syntax_clean_list_a
+    	main_df=pd.concat([main_df,df])
+    	time.sleep(5)
+    return main_df
+
+
+
+
+########### OLD FUNCTION BELOW
+
+
+# def day_injuries_basketball(start_day, end_day) -> pd.DataFrame():
+# def day_injuries_basketball(day) -> pd.DataFrame():
+#     url=f"https://www.prosportstransactions.com/basketball/Search/SearchResults.php?Player=&Team=&BeginDate={day}&EndDate={day}&ILChkBx=yes&InjuriesChkBx=yes&PersonalChkBx=yes&DisciplinaryChkBx=yes&LegalChkBx=yes&Submit=Search"
+#     res=requests.get(url,timeout=None).content
+#     data=BeautifulSoup(res,'html.parser')
+#     table=data.find('table', class_='datatable center')
+#     rows=table.find_all('tr')
+#     mylist=[]
+#     for row in rows:
+#         cols=row.find_all('td')
+#         cols=[x.text.strip() for x in cols]
+#         mylist.append(cols)
+#     df=pd.DataFrame(mylist[1:],columns=['date','team','acquired','relinquished','notes'])
+#     clean_r=[x.replace('• ','') for x in df.relinquished]
+#     clean_a=[x.replace('• ','') for x in df.acquired]
+
+#     syntax_clean_list_r=[]
+#     for name in range(len(clean_r)):
+#         c_name=clean_r[name]
+#         s=c_name.encode().decode('unicode_escape').encode('raw_unicode_escape')
+#         string_uni=s.decode('utf-8')
+#         syntax_clean_list_r.append(string_uni)
+#     df.relinquished=syntax_clean_list_r
+
+#     syntax_clean_list_a=[]
+#     for name in range(len(clean_a)):
+#         c_name=clean_a[name]
+#         s=c_name.encode().decode('unicode_escape').encode('raw_unicode_escape')
+#         string_uni=s.decode('utf-8')
+#         syntax_clean_list_a.append(string_uni)
+#     df.acquired=syntax_clean_list_a
+
+#     return df
