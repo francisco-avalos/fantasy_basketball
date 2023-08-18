5a00b38c24e91d5a0394c50a437b046f6b64018c
francisco-avalos <avalosjr.francisco@gmail.com>
2023-08-18 14:20:21 -0700

    adding additional files

---
+++ test.py
@@ -0,0 +1,12 @@
+import os
+
+sports_db_admin_host = os.environ['sports_db_admin_host']
+sports_db_admin_port = os.environ['sports_db_admin_port']
+sports_db_admin_pw = os.environ['sports_db_admin_pw']
+sports_db_admin_user = os.environ['sports_db_admin_user']
+
+
+print(sports_db_admin_host)
+print(sports_db_admin_port)
+print(sports_db_admin_pw)
+print(sports_db_admin_user)
