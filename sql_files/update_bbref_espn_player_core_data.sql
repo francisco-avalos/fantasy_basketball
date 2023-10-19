bdac6a32965fdbe1ef42fc3dffab918824727e65
francisco-avalos <avalosjr.francisco@gmail.com>
2023-10-18 22:20:34 -0700

    tracking source code for bbref/espn core player data

---
+++ sql_files/update_bbref_espn_player_core_data.sql
@@ -0,0 +1,34 @@
+
+# Reference this repo as the init for behind the R pull 
+# https://github.com/djblechn-su/nba-player-team-ids/tree/master
+
+# 671
+SELECT COUNT(*)
+FROM basketball.basketball_references_players
+LIMIT 100;
+
+# 671
+SELECT COUNT(*)
+FROM basketball.espn_players
+LIMIT 100
+;
+
+TRUNCATE basketball.basketball_references_players;
+TRUNCATE basketball.espn_players;
+
+
+LOAD DATA LOCAL INFILE '/Users/franciscoavalosjr/Desktop/basketball_yahoo_player_info/basketball_yahoo_player_info_directory/BBRefIDs.csv'
+REPLACE INTO TABLE basketball.basketball_references_players FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE '/Users/franciscoavalosjr/Desktop/basketball_yahoo_player_info/basketball_yahoo_player_info_directory/ESPNIDs.csv'
+REPLACE INTO TABLE basketball.espn_players FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' IGNORE 1 ROWS;
+
+
+SELECT *
+FROM basketball.basketball_references_players
+LIMIT 100;
+
+
+SELECT *
+FROM basketball.espn_players
+LIMIT 100;
