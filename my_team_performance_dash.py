ccd623e1c54d7466f20caf4d7e391a1c58e2b32b
francisco-avalos <avalosjr.francisco@gmail.com>
2023-11-07 15:07:04 -0800

    adding correct config vars names

--- my_team_performance_dash.py
+++ my_team_performance_dash.py
@@ -24,11 +24,13 @@ import yahoo_fantasy_api as yfa
 from espn_api.basketball import League
 
 
-sports_db_admin_host=os.environ.get('sports_db_admin_host')
-sports_db_admin_db='basketball'
-sports_db_admin_user=os.environ.get('sports_db_admin_user')
-sports_db_admin_pw=os.environ.get('sports_db_admin_pw')
-sports_db_admin_port=os.environ.get('sports_db_admin_port')
+
+sports_db_admin_host=os.environ.get('basketball_host')
+sports_db_admin_db=os.environ.get('basketball_db')
+sports_db_admin_user=os.environ.get('basketball_user')
+sports_db_admin_pw=os.environ.get('basketball_pw')
+sports_db_admin_port=os.environ.get('basketball_port')
+
 
 leagueid=os.environ.get('leagueid')
 espn_s2=os.environ.get('espn_s2')
