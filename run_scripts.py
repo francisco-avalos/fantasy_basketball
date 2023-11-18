99b153393b2f46d25de8fd8381c3b2120efa88d9
francisco-avalos <avalosjr.francisco@gmail.com>
2023-11-18 14:43:44 -0800

    added back adv-stats script to short refresh + added script name print out at pre-execution

--- run_scripts.py
+++ run_scripts.py
@@ -7,18 +7,18 @@ from subprocess import call
 
 
 chime.theme('mario')
-small_list=['high_level_nba_team_schedules.py',
+small_list=['advanced_stats.py','high_level_nba_team_schedules.py',
 				'high_level_nba_team_stats.py', 'my_team_stats.py',
 				'historicals.py', 'historical_basketball_injury_data.py',
 				'injury_probabilities_basketball.py',
 				'free_agents_yahoo_basketball.py']
-full_list=['advanced_stats.py', 'high_level_nba_team_schedules.py',
+full_list=['advanced_stats.py','high_level_nba_team_schedules.py',
 				'high_level_nba_team_stats.py', 'my_team_stats.py',
 				'historicals.py',
 				'historical_basketball_injury_data.py',
 				'injury_probabilities_basketball.py',
 				'free_agents_yahoo_basketball.py',
-				'free_agents_backfill.py']	
+				'free_agents_backfill.py']
 
 run_fa = input('Execute Free Agents Script?(yes/no) - ')
 
@@ -27,6 +27,7 @@ if run_fa=='yes':
 		print('Begin full list now... \n')
 		for script in full_list:
 			start_time=time.perf_counter()
+			print(f'Preparing to run: {script}')
 			call(['python', script])
 			end_time=time.perf_counter()
 			lapsed_time_min=(end_time-start_time)/60
@@ -43,6 +44,7 @@ elif run_fa=='no':
 		print('Begin small list now... \n')
 		for script in small_list:
 			start_time=time.perf_counter()
+			print(f'Preparing to run: {script}')
 			call(['python', script])
 			end_time=time.perf_counter()
 			lapsed_time_min=(end_time-start_time)/60
