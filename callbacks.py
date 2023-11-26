811553e5c8fe32b290fdc5a537a0c6448e6e6ebc
francisco-avalos <avalosjr.francisco@gmail.com>
2023-11-26 13:23:14 -0800

    second prod config vars in callback file

--- callbacks.py
+++ callbacks.py
@@ -448,19 +448,19 @@ def graph_update(input_value,focus_field_value, calc_value,display_field, top_n_
 
 
 # prod env 
-# sports_db_admin_host=os.environ.get('basketball_host')
-# sports_db_admin_db=os.environ.get('basketball_db')
-# sports_db_admin_user=os.environ.get('basketball_user')
-# sports_db_admin_pw=os.environ.get('basketball_pw')
-# sports_db_admin_port=os.environ.get('basketball_port')
+sports_db_admin_host=os.environ.get('basketball_host')
+sports_db_admin_db=os.environ.get('basketball_db')
+sports_db_admin_user=os.environ.get('basketball_user')
+sports_db_admin_pw=os.environ.get('basketball_pw')
+sports_db_admin_port=os.environ.get('basketball_port')
 
 
 # dev env
-sports_db_admin_host=os.environ.get('sports_db_admin_host')
-sports_db_admin_db='basketball'
-sports_db_admin_user=os.environ.get('sports_db_admin_user')
-sports_db_admin_pw=os.environ.get('sports_db_admin_pw')
-sports_db_admin_port=os.environ.get('sports_db_admin_port')
+# sports_db_admin_host=os.environ.get('sports_db_admin_host')
+# sports_db_admin_db='basketball'
+# sports_db_admin_user=os.environ.get('sports_db_admin_user')
+# sports_db_admin_pw=os.environ.get('sports_db_admin_pw')
+# sports_db_admin_port=os.environ.get('sports_db_admin_port')
 
 
 leagueid=os.environ.get('leagueid')
