7fa2c4e07b682ee2ba408ac64aa567708e1daa47
francisco-avalos <avalosjr.francisco@gmail.com>
2024-04-15 21:35:02 -0700

    update model_evaluation table

--- sql_files/table_creation_definitions.sql
+++ sql_files/table_creation_definitions.sql
@@ -509,14 +509,15 @@
 ########################################################################################################################
 ########################################################################################################################
 
-DROP TABLE IF EXISTS basketball.model_evaluation;
-CREATE TABLE basketball.model_evaluation
-(
-  `league` VARCHAR(50),
-  `slug` VARCHAR(50) NOT NULL,
-  `model_type` VARCHAR(20),
-  `evaluation_metric` VARCHAR(20),
-  `evaluation_metric_value` DECIMAL(5,3),
-  PRIMARY KEY (`league`, `slug`,`model_type`,`evaluation_metric`)
-);
+-- DROP TABLE IF EXISTS basketball.model_evaluation;
+-- CREATE TABLE basketball.model_evaluation
+-- (
+--   `league` VARCHAR(50),
+--   `slug` VARCHAR(50) NOT NULL,
+--   `model_type` VARCHAR(20),
+--   `evaluation_metric` VARCHAR(20),
+--   `evaluation_metric_value` DECIMAL(5,3),
+--   `champion_model` INT, 
+--   PRIMARY KEY (`league`, `slug`,`model_type`,`evaluation_metric`)
+-- );
 
